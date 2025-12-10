977143f8dc4a0c48e166f666e2527079e3a951c3
辛京爵 <xinjingjue@ganweisoft.com>
2025-12-10 14:33:56 +0800

    feat:add setup extension

---
+++ components/Daibitx.EFCore.Extension/Setup/EFCoreServiceCollectionExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Daibitx.EFCore.Extension.Interceptors;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Daibitx.EFCore.Extension.Setup
+{
+    public static class EFCoreServiceCollectionExtensions
+    {
+        public static IServiceCollection AddDaibitxEFCore(this IServiceCollection services)
+        {
+            services.AddSingleton<AuditingInterceptor>();
+            services.AddSingleton<SoftDeleteInterceptor>();
+            services.AddSingleton<LoggingInterceptor>();
+
+            return services;
+        }
+
+        public static DbContextOptionsBuilder AddDaibitxInterceptors(
+            this DbContextOptionsBuilder options,
+            IServiceProvider sp)
+        {
+            return options
+                .AddInterceptors(sp.GetRequiredService<AuditingInterceptor>())
+                .AddInterceptors(sp.GetRequiredService<SoftDeleteInterceptor>())
+                .AddInterceptors(sp.GetRequiredService<LoggingInterceptor>());
+        }
+    }
+}
